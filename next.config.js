943d3cbc5840a5401e51a54b7db869df375d5a48
Smuli <samuli.kakonen@metropolia.fi>
2023-09-27 11:04:14 +0300

    Added image remote patterns

--- next.config.js
+++ next.config.js
@@ -14,6 +14,18 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'scontent-cdg4-1.xx.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-iad3-2.xx.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
